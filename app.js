616a236a3439dfc9faa0d241ab12f714b3692ff4
Patrick <patrickrice@live.com>
2019-04-08 19:47:54 -0500

    setting up initial functions

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+
+const url = 'https://www.cnn.com/';
+
+const responseData = (response) => {
+    return response.data;
+};
+
+const logError = (error) => {
+    console.log(error);
+}
+
+let getHeadlines = html => {
+    data = [];
+    const $ = cheerio.load(html);
+    $('h3').each((i, elem) => {
+        data.push({
+            title: $(elem).text(),
+            link: $(elem).find('a.DY5T1d').attr('href')
+        });
+    });
+    console.log(`This is our data: ${data}`);
+}
+
+axios.get(url)
+    .then(responseData)
+    .then(getHeadlines)
+    .catch(logError);
